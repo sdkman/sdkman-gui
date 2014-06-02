5aaf2cabce3413a6748d2af044b90afd2259a8c5
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-06-02 22:02:28 +0200

    Tweaks to view.

--- griffon-app/views/gvm/gui/GvmGuiView.groovy
+++ griffon-app/views/gvm/gui/GvmGuiView.groovy
@@ -1,19 +1,35 @@
 package gvm.gui
 
 application(title: 'gvm-gui',
-  preferredSize: [320, 240],
-  pack: true,
-  //location: [50,50],
-  locationByPlatform: true,
-  iconImage:   imageIcon('/griffon-icon-48x48.png').image,
-  iconImages: [imageIcon('/griffon-icon-48x48.png').image,
-               imageIcon('/griffon-icon-32x32.png').image,
-               imageIcon('/griffon-icon-16x16.png').image]) {
+        preferredSize: [640, 400],
+        pack: true,
+        //location: [50,50],
+        locationByPlatform: true,
+        iconImage: imageIcon('/griffon-icon-48x48.png').image,
+        iconImages: [imageIcon('/griffon-icon-48x48.png').image,
+                     imageIcon('/griffon-icon-32x32.png').image,
+                     imageIcon('/griffon-icon-16x16.png').image]) {
     migLayout(layoutConstraints: 'fill', columnConstraints: '[40%][]')
-    scrollPane(preferredSize: [320, 160]) {
+    scrollPane(preferredSize: [320, 300]) {
         table {
-            tableFormat = defaultTableFormat(columnNames: ['Name'])
+            tableFormat = defaultAdvancedTableFormat(
+                    columns: [
+                            [
+                                    name : 'name',
+                                    class: String
+                            ]
+                    ]
+            )
+            //columnModel = columnModel {
+            //    column("name", modelIndex: 0, headerValue: "Candidates", width: [50, 100])
+            //}
             eventTableModel(source: model.candidates, format: tableFormat)
+        }
+    }
+    scrollPane(preferredSize: [320, 300]) {
+        table {
+            tableFormat = defaultTableFormat(columnNames: ['Version'])
+            eventTableModel(source: model.versions, format: tableFormat)
         }
     }
 }
