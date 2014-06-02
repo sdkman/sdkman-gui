73b7461e73015b85e9d6a6c1b7357440cd894d6c
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-06-02 22:02:09 +0200

    Adds versions and selected version to model.

--- griffon-app/models/gvm/gui/GvmGuiModel.groovy
+++ griffon-app/models/gvm/gui/GvmGuiModel.groovy
@@ -3,8 +3,14 @@ package gvm.gui
 import ca.odell.glazedlists.BasicEventList
 import ca.odell.glazedlists.EventList
 import net.gvmtool.client.Candidate
+import net.gvmtool.client.Version
 
 class GvmGuiModel {
-    @Bindable Candidate selectedCandidate
+    @Bindable
+    Candidate selectedCandidate
     EventList<Candidate> candidates = new BasicEventList<>()
+
+    @Bindable
+    Version selectedVersion
+    EventList<Version> versions = new BasicEventList<>()
 }
